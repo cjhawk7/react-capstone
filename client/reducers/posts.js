a95f47602dfc64f945d80fbb9ff287c90c5da144
Christopher Johnson <cjhawk7@hotmail.com>
2018-07-05 16:32:22 -0700

    updated photo compoment

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,21 +1,17 @@
-function posts(state=[], action) {
+function posts(state = [], action) {
     switch(action.type) {
-        case 'INCREMENT_LIKES' :
-        console.log('incrementing Likes');
+      case 'INCREMENT_LIKES' :
+        console.log("Incrementing Likes!!");
         const i = action.index;
         return [
-            ...state.slice(0, i),
-            {...state[i], likes: state[i].likes + 1},
-            ...state.slice(i + 1),     
+          ...state.slice(0,i), // before the one we are updating
+          {...state[i], likes: state[i].likes + 1},
+          ...state.slice(i + 1), // after the one we are updating
         ]
-        default:
-            return state;
+      default:
+        return state;
     }
-
-
-    console.log(state, action);
-    return state;
-}
-console.log("the post will change");
-console.log("state, action");
-export default posts;
+  }
+  
+  export default posts;
+  
